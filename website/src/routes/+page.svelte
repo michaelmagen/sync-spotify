4f5c86265ee6f14caa0b3aa377f010dd6f0edda1
Michael Magen <michael.magen101@gmail.com>
2023-11-07 15:45:18 -0800

    Add shadcn ui

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Button class="w-40" variant="default">default</Button>
+<Button variant="link" href="https://github.com/michaelmagen/sync-spotify">Link to repo</Button>
+<Button variant="destructive">destructive</Button>
+<Button variant="outline">outline</Button>
+<Button variant="secondary">secondary</Button>
+<Button variant="ghost">ghost</Button>
